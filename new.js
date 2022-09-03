d2ffd159aa69cc785aaa2de7452b70c3a3d10f76
yukazu211 <104954232+yukazu211@users.noreply.github.com>
2022-09-03 20:47:15 +0900

    Update new.js

--- new.js
+++ new.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-var newnum1 = "0003"
-var newtit1 = "N700系Sが搬入される"
-var newnum2 = "0002"
-var newtit2 = "身延線に臨時列車が運転"
-var newnum3 = "0001"
-var newtit3 = "2022年静ｼｽのNG出場まとめ"
-var newnum4 = ""
-var newtit4 = ""
+var newnum1 = "0003";
+var newtit1 = "N700系Sが搬入される";
+var newnum2 = "0002";
+var newtit2 = "身延線に臨時列車が運転";
+var newnum3 = "0001";
+var newtit3 = "2022年静ｼｽのNG出場まとめ";
+var newnum4 = "";
+var newtit4 = "";
 
 document.getElementById('head').innerHTML = '<div class="side-c" id="head"> <div class="head-c"><a href="../blog" class="he header-c"><b>最新記事</b></a></div>'+
 '<a href="../blog/'+newnum1+'" class="news"><div class="news"><img src="../blog/'+newnum1+ '/header.jpeg" class="newsimg"><b class="news">'+newtit1+'</b></div></a>'      +
