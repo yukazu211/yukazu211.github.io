e546be0b0390931c8e309bee41695d0493bb6b62
yukazu211 <104954232+yukazu211@users.noreply.github.com>
2022-09-03 23:34:49 +0900

    Update new.js

--- new.js
+++ new.js
@@ -1,12 +1,24 @@
 window.onload = function() {
 var newnum1 = "0003";
 var newtit1 = "N700系Sが搬入される";
-var newnum2 = "0002";
-var newtit2 = "身延線に臨時列車が運転";
-var newnum3 = "0001";
-var newtit3 = "2022年静ｼｽのNG出場まとめ";
+var newtim1 = "2022/09/03";
+var newkos1 = "none";
+var newhon1 = "<p>8月24日から9月1日まで東海道新幹線のN700SのJ32編成が、愛知県の豊川市にある日本車輌製造から浜松市にあるJR東海の浜松工場まで搬入されました。今後のN700Sの活躍に期待したいです。<br><br>撮影した日は、7号車と8号車が搬入されました。<br><br> <img src=\"https://pbs.twimg.com/media/FbohhltaIAA9Fo2?format=jpg&name=medium\" class=\"img50\"><img src=\"https://pbs.twimg.com/media/FbohhltaQAAmQwy?format=jpg&name=medium\" class=\"img50\"> </p><h4>搬入の様子</h4> <video src=\"../blog/0003/video1.mp4\"style=\"width:100%;\" controls poster=\"../blog/0003/video2.png\"></video>";
+var newnum2 = "0001";
+var newtit2 = "2022年静ｼｽのNG出場まとめ";
+var newtim2 = "2022/08/14";
+var newkos2 = "2022/09/01";
+var newhon2 = "<ul> <li>これはTwitterを元に作成したため間違いがある可能性があります。</li> <li>もし、間違い等ございましたら、<a href=\"./send\">フォーム</a>に送っていただくと助かります。</li> <li>このサイトの編成記号で使用している色は、<a href=\"https://twitter.com/hustler_tora\" target=\"_blank\">@hustler_tora</a>さんの<a href=\"https://tokaido-unyo-shizuoka.com/unyo/model_abstract.html\" target=\"_blank\">編成モデル概要</a>を参考にしています。</li> </ul> <table> <tr> <td dir=\"LTR\">8月25日</td> <td dir=\"LTR\"class=\"T313\">T8</td> </tr> <tr> <td dir=\"LTR\">8月17日</td> <td dir=\"LTR\"class=\"V313\">V13</td> </tr> <tr> <td dir=\"LTR\">8月5日</td> <td dir=\"LTR\" class=\"W313\">W8</td> </tr> <tr> <td dir=\"LTR\">8月3日</td> <td dir=\"LTR\"class=\"LL211\">LL6</td> </tr> <tr> <td dir=\"LTR\">7月26日</td> <td dir=\"LTR\"class=\"V313\">V14</td> </tr> <tr> <td dir=\"LTR\">7月13日</td> <td dir=\"LTR\"class=\"W313\">W6</td> </tr> <tr> <td dir=\"LTR\">7月1日</td> <td dir=\"LTR\"class=\"V313\">V5</td> </tr> <tr> <td dir=\"LTR\">6月10日</td> <td dir=\"LTR\"class=\"W313\">W7</td> </tr> <tr> <td dir=\"LTR\">6月1日</td> <td dir=\"LTR\"class=\"GG211\">GG3</td> </tr> <tr> <td dir=\"LTR\">5月23日</td> <td dir=\"LTR\"class=\"T313\">T7</td> </tr> <tr> <td dir=\"LTR\">5月17日</td> <td dir=\"LTR\"class=\"V313\">V11</td> </tr> <tr> <td dir=\"LTR\">4月27日</td> <td dir=\"LTR\"class=\"N313\">N2</td> </tr> <tr> <td dir=\"LTR\">4月14日</td> <td dir=\"LTR\"class=\"LL211\">LL18</td> </tr> <tr> <td dir=\"LTR\">4月11日</td> <td dir=\"LTR\"class=\"T313\">T6</td> </tr> <tr> <td dir=\"LTR\">3月31日</td> <td dir=\"LTR\"class=\"SS211\">SS11</td> </tr> <tr> <td dir=\"LTR\">3月29日</td> <td dir=\"LTR\"class=\"GG211\">GG6</td> </tr> <tr> <td dir=\"LTR\">3月22日</td> <td dir=\"LTR\"class=\"V313\">V8</td> </tr> <tr> <td dir=\"LTR\">3月16日</td> <td dir=\"LTR\"class=\"T313\">T5</td> </tr> <tr> <td dir=\"LTR\">3月2日</td> <td dir=\"LTR\"class=\"S313\">B204(現：S4)</td> </tr> <tr> <td dir=\"LTR\">2月17日</td> <td dir=\"LTR\"class=\"N313\">N3</td> </tr> <tr> <td dir=\"LTR\">2月15日</td> <td dir=\"LTR\"class=\"SS211\">SS3</td> </tr> <tr> <td dir=\"LTR\">2月8日</td> <td dir=\"LTR\"class=\"GG211\">GG1</td> </tr> <tr> <td dir=\"LTR\">1月28日</td> <td dir=\"LTR\"class=\"W313\">W5</td> </tr> <tr> <td dir=\"LTR\">1月13日</td> <td dir=\"LTR\"class=\"LL211\">LL13</td> </tr> </table><br>";
+var newnum3 = "0002";
+var newtit3 = "身延線に臨時列車が運転";
+var newtim3 = "2022/08/14";
+var newkos3 = "none";
+var newhon3 = "<p>８月７日(日)に沿線で神明花火が３年ぶりに開催されました。そのため、身延線では今年も臨時列車が運転されました。今回は、大垣車両区に所属している313系5300番台のZ5編成が静岡地区に貸し出し、臨時列車として313系のＷ編成との４両編成で走行しました。Z５編成や基本的には走行しない313系の8000番台が走り、転換クロスシートの車両が多く見られました。<br><br>下記の画像は身延線の時刻表です。<br><br> <div class=\"cursorplus\"><a href=\"../blog/0002/1.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/1.jpeg\" class=\"img50\"></a><a href=\"../blog/0002/2.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/2.jpeg\" class=\"img50\"></a><a href=\"../blog/0002/3.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/3.jpeg\" class=\"img50\"></a><a href=\"../blog/0002/4.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/4.jpeg\" class=\"img50\"></div> </p>";
 var newnum4 = "";
 var newtit4 = "";
+var newtim4 = "";
+var newkos4 = "none";
+var newhon4 = "a";
 
 document.getElementById('head').innerHTML = '<div class="side-c" id="head"> <div class="head-c"><a href="../blog" class="he header-c"><b>最新記事</b></a></div>'+
 '<a href="../blog/'+newnum1+'" class="news"><div class="news"><img src="../blog/'+newnum1+ '/header.jpeg" class="newsimg"><b class="news">'+newtit1+'</b></div></a>'      +
@@ -14,7 +26,149 @@ document.getElementById('head').innerHTML = '<div class="side-c" id="head"> <div
 '<a href="../blog/'+newnum3+'" class="news"><div class="news"><img src="../blog/'+newnum3+ '/header.jpeg" class="newsimg"><b class="news">'+newtit3+'</b></div></a>'      +
 // '<a href="../blog/'+newnum4+'" class="news"><div class="news"><img src="../blog/'+newnum4+ '/header.jpeg" class="newsimg"><b class="news">'+newtit4+'</b></div></a>'      +
   '</div>';
- var decode = decodeURI(location.href);	  
-	  
-document.getElementById('url').innerHTML ='<a href="' + decode +'">' + decode + '</a>' + 'は、見つかりませんでした。<br>５秒後にトップページに戻ります。';
+
+
+var url = document.getElementById('url');
+
+if (url){ //404page用
+     var decode = decodeURI(location.href);
+  
+url.innerHTML ='<a href="' + decode +'">' + decode + '</a>' + 'は、見つかりませんでした。<br>５秒後にトップページに戻ります。';
+} else {
+
+      console.log('ない');
 }
+  
+if (newkos1=="none"){
+  var newcha1 = '';
+} else {
+ var newcha1 = '　<span id="change1"></span> '+ newkos1;
+}
+if (newkos2=="none"){
+  var newcha2 = '';
+} else {
+ var newcha2 = '　<span id="change2"></span> '+ newkos2;
+}
+if (newkos3=="none"){
+  var newcha3 = '';
+} else {
+ var newcha3 = '　<span id="change3"></span> '+ newkos3;
+}
+if (newkos4=="none"){
+  var newcha4 = '';
+} else {
+ var newcha4 = '　<span id="change4"></span> '+ newkos4;
+}
+
+  
+var newskiji = document.getElementById('newskiji');
+
+if (newskiji){
+newskiji.innerHTML = '<p><span id="time1"></span>'+newtim1+newcha1+'<a href="./blog/'+newnum1+'"><br><b class="newstitle">'+newtit1+'</b></a></p><div>'+newhon1+'</div><hr>'+
+'<p><span id="time2"></span>'+newtim2+newcha2+'<a href="./blog/'+newnum2+'"><br><b class="newstitle">'+newtit2+'</b></a></p><div>'+newhon2+'</div><hr>'+
+'<p><span id="time3"></span>'+newtim3+newcha3+'<a href="./blog/'+newnum3+'"><br><b class="newstitle">'+newtit3+'</b></a></p><div>'+newhon3+'</div><hr>'
+//+ '<p><span id="time4"></span>'+newtim4+newcha4+'<a href="./blog/'+newnum4+'"><br><b class="newstitle">'+newtit4+'</b></a></p><div>'+newhon4+'</div><hr>'
+;
+} else {
+
+      console.log('ない');
+}
+  
+
+    
+var time = document.getElementById('time');
+
+if (time){
+time.innerHTML = '<img src="https://www.svgrepo.com/show/13698/time.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}
+      
+var time = document.getElementById('time1');
+
+if (time){
+time.innerHTML = '<img src="https://www.svgrepo.com/show/13698/time.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}
+
+      
+var time = document.getElementById('time2');
+
+if (time){
+time.innerHTML = '<img src="https://www.svgrepo.com/show/13698/time.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}
+      
+var time = document.getElementById('time3');
+
+if (time){
+time.innerHTML = '<img src="https://www.svgrepo.com/show/13698/time.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}
+    
+var time = document.getElementById('time4');
+
+if (time){
+time.innerHTML = '<img src="https://www.svgrepo.com/show/13698/time.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}
+  
+  
+  
+var change = document.getElementById('change');
+
+if (change){
+change.innerHTML = '<img src="https://www.svgrepo.com/show/381214/refresh-reload-update-repeat-rotate.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}  
+    
+var change = document.getElementById('change1');
+
+if (change){
+change.innerHTML = '<img src="https://www.svgrepo.com/show/381214/refresh-reload-update-repeat-rotate.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}  
+    
+var change = document.getElementById('change2');
+
+if (change){
+change.innerHTML = '<img src="https://www.svgrepo.com/show/381214/refresh-reload-update-repeat-rotate.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}  
+    
+var change = document.getElementById('change3');
+
+if (change){
+change.innerHTML = '<img src="https://www.svgrepo.com/show/381214/refresh-reload-update-repeat-rotate.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}  
+    
+var change = document.getElementById('change4');
+
+if (change){
+change.innerHTML = '<img src="https://www.svgrepo.com/show/381214/refresh-reload-update-repeat-rotate.svg" class="times"> '
+} else {
+
+      console.log('ない');
+}  
+    
+}
+
+
