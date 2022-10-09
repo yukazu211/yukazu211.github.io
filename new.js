e498919c436e8c391ec6ad6d8a999fb04011715f
ゆうかず <104954232+yukazu211@users.noreply.github.com>
2022-10-09 21:10:37 +0900

    Update new.js

--- new.js
+++ new.js
@@ -2,22 +2,19 @@ var newnum1 = "0001";
 var newtit1 = "2022年静ｼｽのNG出場まとめ";
 var newtim1 = "2022/08/14";
 var newkos1 = "2022/09/28";
-var newhon1 = "<ul> <li>これはTwitterを元に作成したため間違いがある可能性があります。</li> <li>もし、間違い等ございましたら、<a href=\"./send\">フォーム</a>に送っていただくと助かります。</li> <li>このサイトの編成記号で使用している色は、<a href=\"https://twitter.com/hustler_tora\" target=\"_blank\">@hustler_tora</a>さんの<a href=\"https://tokaido-unyo-shizuoka.com/unyo/model_abstract.html\" target=\"_blank\">編成モデル概要</a>を参考にしています。</li> </ul> <table> <tr> <td dir=\"LTR\">9月28日</td> <td dir=\"LTR\"class=\"F373\">F6</td> </tr><tr> <td dir=\"LTR\">9月9日</td> <td dir=\"LTR\"class=\"SS211\">SS8</td> <tr> <td dir=\"LTR\">9月5日</td> <td dir=\"LTR\"class=\"W313\">W1</td> </tr> <tr> <td dir=\"LTR\">8月25日</td> <td dir=\"LTR\"class=\"T313\">T8</td> </tr> <tr> <td dir=\"LTR\">8月17日</td> <td dir=\"LTR\"class=\"V313\">V13</td> </tr> <tr> <td dir=\"LTR\">8月5日</td> <td dir=\"LTR\" class=\"W313\">W8</td> </tr> <tr> <td dir=\"LTR\">8月3日</td> <td dir=\"LTR\"class=\"LL211\">LL6</td> </tr> <tr> <td dir=\"LTR\">7月26日</td> <td dir=\"LTR\"class=\"V313\">V14</td> </tr> <tr> <td dir=\"LTR\">7月13日</td> <td dir=\"LTR\"class=\"W313\">W6</td> </tr> <tr> <td dir=\"LTR\">7月1日</td> <td dir=\"LTR\"class=\"V313\">V5</td> </tr> <tr> <td dir=\"LTR\">6月10日</td> <td dir=\"LTR\"class=\"W313\">W7</td> </tr> <tr> <td dir=\"LTR\">6月1日</td> <td dir=\"LTR\"class=\"GG211\">GG3</td> </tr> <tr> <td dir=\"LTR\">5月23日</td> <td dir=\"LTR\"class=\"T313\">T7</td> </tr> <tr> <td dir=\"LTR\">5月17日</td> <td dir=\"LTR\"class=\"V313\">V11</td> </tr> <tr> <td dir=\"LTR\">4月27日</td> <td dir=\"LTR\"class=\"N313\">N2</td> </tr> <tr> <td dir=\"LTR\">4月14日</td> <td dir=\"LTR\"class=\"LL211\">LL18</td> </tr> <tr> <td dir=\"LTR\">4月11日</td> <td dir=\"LTR\"class=\"T313\">T6</td> </tr> <tr> <td dir=\"LTR\">3月31日</td> <td dir=\"LTR\"class=\"SS211\">SS11</td> </tr> <tr> <td dir=\"LTR\">3月29日</td> <td dir=\"LTR\"class=\"GG211\">GG6</td> </tr> <tr> <td dir=\"LTR\">3月22日</td> <td dir=\"LTR\"class=\"V313\">V8</td> </tr> <tr> <td dir=\"LTR\">3月16日</td> <td dir=\"LTR\"class=\"T313\">T5</td> </tr> <tr> <td dir=\"LTR\">3月2日</td> <td dir=\"LTR\"class=\"S313\">B204(現：S4)</td> </tr> <tr> <td dir=\"LTR\">2月17日</td> <td dir=\"LTR\"class=\"N313\">N3</td> </tr> <tr> <td dir=\"LTR\">2月15日</td> <td dir=\"LTR\"class=\"SS211\">SS3</td> </tr> <tr> <td dir=\"LTR\">2月8日</td> <td dir=\"LTR\"class=\"GG211\">GG1</td> </tr> <tr> <td dir=\"LTR\">1月28日</td> <td dir=\"LTR\"class=\"W313\">W5</td> </tr> <tr> <td dir=\"LTR\">1月13日</td> <td dir=\"LTR\"class=\"LL211\">LL13</td> </tr> </table><br>";
 var newnum2 = "0003";
 var newtit2 = "N700系Sが搬入される";
 var newtim2 = "2022/09/03";
 var newkos2 = "none";
-var newhon2 = "<p>8月24日から9月1日まで東海道新幹線のN700SのJ32編成が、愛知県の豊川市にある日本車輌製造から浜松市にあるJR東海の浜松工場まで搬入されました。今後のN700Sの活躍に期待したいです。<br><br>撮影した日は、7号車と8号車が搬入されました。<br><br> <img src=\"https://pbs.twimg.com/media/FbohhltaIAA9Fo2?format=jpg&name=medium\" class=\"img50\"><img src=\"https://pbs.twimg.com/media/FbohhltaQAAmQwy?format=jpg&name=medium\" class=\"img50\"> </p><h4>搬入の様子</h4> <video src=\"../blog/0003/video1.mp4\"style=\"width:100%;\" controls poster=\"../blog/0003/video2.png\"></video>";
 var newnum3 = "0002";
 var newtit3 = "身延線に臨時列車が運転";
 var newtim3 = "2022/08/14";
 var newkos3 = "none";
-var newhon3 = "<p>８月７日(日)に沿線で神明花火が３年ぶりに開催されました。そのため、身延線では今年も臨時列車が運転されました。今回は、大垣車両区に所属している313系5300番台のZ5編成が静岡地区に貸し出し、臨時列車として313系のＷ編成との４両編成で走行しました。Z５編成や基本的には走行しない313系の8000番台が走り、転換クロスシートの車両が多く見られました。<br><br>下記の画像は身延線の時刻表です。<br><br> <div class=\"cursorplus\"><a href=\"../blog/0002/1.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/1.jpeg\" class=\"img50\"></a><a href=\"../blog/0002/2.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/2.jpeg\" class=\"img50\"></a><a href=\"../blog/0002/3.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/3.jpeg\" class=\"img50\"></a><a href=\"../blog/0002/4.jpeg\" data-lightbox=\"abc\" data-title=\"\"><img src=\"../blog/0002/4.jpeg\" class=\"img50\"></div> </p>";
 var newnum4 = "";
 var newtit4 = "";
 var newtim4 = "";
 var newkos4 = "none";
-var newhon4 = "a";
+
 if(window.matchMedia('(prefers-color-scheme: dark)').matches == true){//OSの色の取得
 	dark();
 }else{
