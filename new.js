8b0ff48aa8446cf6adbf0f8fbd0f8edf77840831
ゆうかず <104954232+yukazu211@users.noreply.github.com>
2022-11-06 22:11:55 +0900

    Update new.js

--- new.js
+++ new.js
@@ -1,7 +1,7 @@
 var newnum1 = "0001";
 var newtit1 = "2022年静ｼｽのNG出場まとめ";
 var newtim1 = "2022/08/14";
-var newkos1 = "2022/11/03";
+var newkos1 = "2022/11/0";
 
 var newnum2 = "0004";
 var newtit2 = "8000番代の重連が走行する";
