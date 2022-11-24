e19515618262b62eadd421e569759dff7a798ca9
ゆうかず <104954232+yukazu211@users.noreply.github.com>
2022-11-24 22:18:47 +0900

    Update new.js

--- new.js
+++ new.js
@@ -1,7 +1,7 @@
 var newnum1 = "0001";
 var newtit1 = "2022年静ｼｽのNG出場まとめ";
 var engtit1 = "Summary that left the Nagoya Plant in 2022";
-var newtim1 = ["2022/08/14", "2022/11/18"];
+var newtim1 = ["2022/08/14", "2022/11/24"];
 
 var newnum2 = "0004";
 var newtit2 = "8000番代の重連が走行する";
